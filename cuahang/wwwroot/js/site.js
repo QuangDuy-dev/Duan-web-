f28de732d9a74cad56a4bec38af631e1b8166678
clogg33448 <clogg33448@gmail.com>
2026-03-31 19:09:49 +0700

    Thêm menu chia sẽ ma thuật chỉ add trước link github

--- cuahang/wwwroot/js/site.js
+++ cuahang/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+})
