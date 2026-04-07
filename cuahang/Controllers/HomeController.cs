b810163b5c628b8e69b535944536df0c57910342
LeoChau7569 <Lamchau7569@gmail.com>
2026-04-07 21:32:47 +0700

    update

--- cuahang/Controllers/HomeController.cs
+++ cuahang/Controllers/HomeController.cs
@@ -11,13 +11,23 @@ namespace cuahang.Controllers
         private readonly ApplicationDbContext _db;
         public HomeController(ApplicationDbContext db) { _db = db; }
 
-        public IActionResult Index(string keyword)
+        public IActionResult Index(string keyword, string type, string brand)
         {
             var dsSanPham = _db.SanPham.Include(s => s.ChiTietHoaDons).AsQueryable();
             if (!string.IsNullOrEmpty(keyword))
             {
                 dsSanPham = dsSanPham.Where(p => p.TenSP.Contains(keyword));
             }
+            if (!string.IsNullOrEmpty(type))
+            {
+                dsSanPham = dsSanPham.Where(p => p.LoaiSp.StartsWith(type));
+            }
+
+            if (!string.IsNullOrEmpty(brand))
+            {
+                dsSanPham = dsSanPham.Where(p => p.LoaiSp.EndsWith(" " + brand));
+            }
+
 
             if (!_db.SanPham.Any())
             {
@@ -42,7 +52,31 @@ namespace cuahang.Controllers
                 _db.SaveChanges();
             }
 
+            var brandGroups = new Dictionary<string, List<dynamic>>
+            {
+                { "1", new List<dynamic> { 
+                    new { Code = "ip", Name = "Apple" }, new { Code = "ss", Name = "Samsung" },
+                    new { Code = "xm", Name = "Xiaomi" }, new { Code = "nb", Name = "Nubia" },
+                    new { Code = "sn", Name = "Sony" }, new { Code = "vv", Name = "Vivo" },
+                    new { Code = "px", Name = "Pixel" }
+                }},
+                { "2", new List<dynamic> { 
+                    new { Code = "ss", Name = "Samsung" }, new { Code = "ak", Name = "Anker" },
+                    new { Code = "tc", Name = "Transcend" }, new { Code = "sd", Name = "Sandisk" },
+                    new { Code = "dji", Name = "DJI" }, new { Code = "ct", Name = "Cuktech" },
+                    new { Code = "atk", Name = "ATK" }, new { Code = "mad", Name = "MAD" },
+                    new { Code = "rz", Name = "Razer" }, new { Code = "cs", Name = "Corsair" },
+                    new { Code = "ip", Name = "Apple" }, new { Code = "lg", Name = "LG" },
+                    new { Code = "asus", Name = "Asus" }, new { Code = "lenovo", Name = "Lenovo" },
+                    new { Code = "msi", Name = "MSI" }, new { Code = "sn", Name = "Sony" }
+                }}
+            };
+
+            ViewBag.DisplayBrands = !string.IsNullOrEmpty(type) && brandGroups.ContainsKey(type) ? brandGroups[type] : new List<dynamic>();
+            ViewBag.CurrentType = type;
+            ViewBag.CurrentBrand = brand;
             return View(dsSanPham.ToList());
+            
         }
 
         public IActionResult Privacy()
